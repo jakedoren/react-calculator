de3ff439244e28c00646c26063bed0c91b4d8e2e
Jacob Doren <Jacob Doren>
2021-11-06 16:39:02 -0400

    clear and operand fix

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,25 +7,25 @@ const Calculator = () => {
         firstNumber: '',
         secondNumber: ''
     })
-    const [firstNum, setFirstNum] = useState()
-    const [secondNum, setSecondNum] = useState()
+    const [onSecondNumber, setOnSecondNumber] = useState(false)
+    const [operatorRequested, setOperatorRequested] = useState()
     
-    // const handleChange = (e) => {
-    //     if(numbersUsed.firstNumber == '') {
-    //         setNumbersUsed({
-    //             firstNumber: e.target.value
-    //         })
-    //     }
-    //     if(numbersUsed != '') {
-    //         setNumbersUsed({
-    //             ...numbersUsed, secondNumber : e.target.value
-    //         })
-    //     }
+    const handleChange = (e) => {
+        // if(numbersUsed.firstNumber == '') {
+        //     setNumbersUsed({
+        //         firstNumber: e.target.value
+        //     })
+        // }
+        // if(numbersUsed != '') {
+        //     setNumbersUsed({
+        //         ...numbersUsed, secondNumber : e.target.value
+        //     })
+        // }
 
-    //     setNumbersUsed({
-    //         ...numbersUsed, [e.target.name] : e.target.value
-    //     })
-    // }
+        setNumbersUsed({
+            ...numbersUsed, [e.target.name] : e.target.value
+        })
+    }
 
     const handleOperator = (e) => {
         e.preventDefault()
@@ -33,38 +33,79 @@ const Calculator = () => {
         const num2 = parseInt(numbersUsed.secondNumber)
         const origin = e.target.getAttribute("name")
 
-        if(numbersUsed.firstNumber == '') {
-            console.log(e.target)
-            setNumbersUsed({
-                firstNumber: e.target.value
-            })
-        } else if (numbersUsed.firstNumber !== '' && numbersUsed.secondNumber == '') {
-            setNumbersUsed({
-                ...numbersUsed, secondNumber: e.target.value
-            })
-        } else {
-            setNumbersUsed({
-                ...numbersUsed, secondNumber: e.target.value
-            })
+        setOperatorRequested(origin)
+
+        if(numbersUsed.firstNumber !== '') {
+            setOnSecondNumber(true)
         }
 
-        if(origin === "add") {
+        // if(numbersUsed.firstNumber !== '') {
+        //     setNumbersUsed({
+        //         ...numbersUsed, secondNumber : document.getElementById("numb").value
+        //     })
+        // }
+
+        // if(numbersUsed.firstNumber == '') {
+        //     console.log(e.target)
+        //     setNumbersUsed({
+        //         firstNumber: e.target.value
+        //     })
+        // } else if (numbersUsed.firstNumber !== '' && numbersUsed.secondNumber == '') {
+        //     setNumbersUsed({
+        //         ...numbersUsed, secondNumber: e.target.value
+        //     })
+        // } else {
+        //     setNumbersUsed({
+        //         ...numbersUsed, secondNumber: e.target.value
+        //     })
+        // }
+
+        // if(origin === "add") {
+        //     const sum = addNum(num1, num2)
+        //     console.log(num1, num2)
+        //     setTotal(sum)
+        // }
+        // if(origin === "subtract") {
+        //     const total = subtractNum(num1, num2)
+        //     setTotal(total)
+        // }
+        // if(origin === "divide") {
+        //     const total = divideNum(num1, num2)
+        //     setTotal(total)
+        // }
+        // if(origin === "multiply") {
+        //     const total = multiplyNum(num1, num2)
+        //     setTotal(total)
+        // }
+    }
+
+    const equalsOperator = (e) => {
+        e.preventDefault()
+        const num1 = parseInt(numbersUsed.firstNumber)
+        const num2 = parseInt(numbersUsed.secondNumber)
+        if(operatorRequested === "add") {
             const sum = addNum(num1, num2)
             console.log(num1, num2)
             setTotal(sum)
         }
-        if(origin === "subtract") {
+        if(operatorRequested === "subtract") {
             const total = subtractNum(num1, num2)
             setTotal(total)
         }
-        if(origin === "divide") {
+        if(operatorRequested === "divide") {
             const total = divideNum(num1, num2)
             setTotal(total)
         }
-        if(origin === "multiply") {
+        if(operatorRequested === "multiply") {
             const total = multiplyNum(num1, num2)
             setTotal(total)
         }
+    }
+
+    const handleClear = () => {
+        setTotal('')
+        setNumbersUsed({ firstNumber: '', secondNumber: '' })
+        setOnSecondNumber(false)
     }
 
     return (
@@ -74,6 +115,8 @@ const Calculator = () => {
             <p onClick={(e) => handleOperator(e)} name="subtract" >-</p>
             <p onClick={(e) => handleOperator(e)} name="divide" >/</p>
             <p onClick={(e) => handleOperator(e)} name="multiply" >x</p>
+            <p onClick={(e) => equalsOperator(e)} >=</p>
+            <p onClick={e => handleClear(e)} >Clear</p>
             <span>1</span>
             <br></br>
             <span>2</span>
@@ -96,8 +139,7 @@ const Calculator = () => {
             {numbersUsed.firstNumber}
             <span>+</span>
             {numbersUsed.secondNumber}
-            <input onClick={(e) => handleOperator(e)} name="firstNumber" placeholder="firstNumb"  ></input>
-            {/* <input onChange={(e) => handleChange(e)} name="secondNumber" placeholder="secondNumb" value={numbersUsed.secondNumber} ></input> */}
+            <input onChange={(e) => handleChange(e)} name={onSecondNumber ? "secondNumber" : "firstNumber"} placeholder="firstNumb" value={onSecondNumber ? numbersUsed.secondNumber : numbersUsed.firstNumber}></input> 
         </form>
     )
 }
